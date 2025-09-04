20c95cad4b916a48fb5f1e26b1fdf84c8e8c84db
crispent <crispentafa@gmail.com>
2025-09-04 13:45:27 +0200

    First CodeDeploy

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,6 +1,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-
+# update
 import json
 import os
 os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
